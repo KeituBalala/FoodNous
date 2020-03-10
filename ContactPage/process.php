3074d82d2bff3769fa10d7658351169d66a8dc5c
thelalakei <thelalakei@gmail.com>
2020-03-10 19:18:47 +0200

    Update process.php

---
+++ ContactPage/process.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <Head>
+        <title>Message Sent</title>
+    </Head>
+    <Body>
+        <?php
+        echo "SUCCESS!"
+        echo "YOUR MESSAGE HAS BEEN SENT"
+        echo "We appreciate your message and will respond as soon as possible."
+        ?>
+    </Body>
+</html>
