4f0736a68a754e8ae384762e20744f496e57f521
thelalakei <thelalakei@gmail.com>
2020-04-26 14:47:46 +0200

    Update index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let vid = document.getElementById("myVideo"); 
-
-const button = document.body.children[1]; console.log(button);
-
-button.addEventListener('Click', function playVid() { 
-  vid.play(); 
-}) 
