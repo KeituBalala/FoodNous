97bda36557895b477dda762051e35b257a772955
thelalakei <thelalakei@gmail.com>
2020-04-26 14:09:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ const button = document.body.children[1]; console.log(button);
 button.addEventListener('Click', function playVid() { 
   vid.play(); 
 } 
-
-const button2 = document.body.children[1]; console.log(button2);
-
-button2.addEventListener('Click',function pauseVid() { 
-  vid.pause(); 
-} 
