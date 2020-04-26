bf9d919c8350b97d9c5c951b738d5fbb5603e902
thelalakei <thelalakei@gmail.com>
2020-04-26 12:35:59 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('button');
+
+function bgChange() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}   
+
+btn.addEventListener('click', bgChange);
