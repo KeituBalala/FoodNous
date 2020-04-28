30b9ca845dda33fedfb839cc73055118c21a20cf
thelalakei <thelalakei@gmail.com>
2020-04-28 17:54:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-  const myVid = document.getElementById(myVid);
-
-  let goFS = document.getElementById("goFS");
-  goFS.addEventListener("click", function() {
-      if (myVid.requestFullscreen) {
-        myVid.requestFullscreen();
-      } else if (myVid.mozRequestFullScreen) { /* Firefox */
-        myVid.mozRequestFullScreen();
-      } else if (myVid.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-        myVid.webkitRequestFullscreen();
-      } else if (myVid.msRequestFullscreen) { /* IE/Edge */
-        myVid.msRequestFullscreen();
+const myVid = document.getElementById("myVid");
+function openFullscreen() {
+  if (myVid.requestFullscreen) {
+    myVid.requestFullscreen();
+  } else if (myVid.mozRequestFullScreen) { /* Firefox */
+    myVid.mozRequestFullScreen();
+  } else if (myVid.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    myVid.webkitRequestFullscreen();
+  } else if (myVid.msRequestFullscreen) { /* IE/Edge */
+    myVid.msRequestFullscreen();
   }
-}
-  
+} 
