14ce5cd098031807dc345be0926c748fe3f7646e
thelalakei <thelalakei@gmail.com>
2020-04-28 18:30:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+  let myVid = document.getElementById("myVid");
+
   let goFS = document.getElementById("goFS");
   goFS.addEventListener("click", function() {
-      document.body.requestFullscreen();
+      document.myVid.requestFullscreen();
   }, false);
   
