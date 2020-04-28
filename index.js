e7c6898f462c4752acf09d50d6a039362fadf8b4
thelalakei <thelalakei@gmail.com>
2020-04-28 18:19:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const myVid = document.getElementById("myVid");
-function openFullscreen() {
-  if (myVid.requestFullscreen) {
-    myVid.requestFullscreen();
-  } else if (myVid.mozRequestFullScreen) { /* Firefox */
-    myVid.mozRequestFullScreen();
-  } else if (myVid.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    myVid.webkitRequestFullscreen();
-  } else if (myVid.msRequestFullscreen) { /* IE/Edge */
-    myVid.msRequestFullscreen();
-  }
-} 
+let myVid = document.getElementById("myVid");
+
+Function myFunction() {
+  myVid.requestFullscreen();
+}
+
+let goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", myFunction();
+  )
+  
