050cddec815b63c17661728de62bc19baa42d397
thelalakei <thelalakei@gmail.com>
2020-04-28 18:25:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-let myVid = document.getElementById("myVid");
-
-Function myFunction() {
-  myVid.requestFullscreen();
-}
-
-let goFS = document.getElementById("goFS");
-  goFS.addEventListener("click", myFunction();
-  )
+  let goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", function() {
+      document.body.requestFullscreen();
+  }, false);
   
