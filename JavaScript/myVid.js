ebea90fb61a13bd0b12996105175284c03e753b5
thelalakei <thelalakei@gmail.com>
2020-05-05 20:48:30 +0200

    Create myVid.js

---
+++ JavaScript/myVid.js
@@ -0,0 +1,6 @@
+  let myVid = document.getElementById("myVid");
+
+  let goFS = document.getElementById("goFS");
+  goFS.addEventListener("click", function() {
+      myVid.requestFullscreen();
+  })
