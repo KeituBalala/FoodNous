253af9af1e80c698c64371bc1d44c7470a70b943
thelalakei <thelalakei@gmail.com>
2020-05-07 15:45:23 +0200

    Update nav.js

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -1,10 +1,10 @@
 const menuItems = [
-    {title: 'HOME', link:'../index.html'},
-    {title: 'ABOUT', link:'../AboutPage/index.html'},
-    {title: 'CHARACTER CONTENT', link:'../CharacterContent/index.html'},
-    {title: 'THEORY', link:'../TheoryContent/index.html'},
-    {title: 'CONTACT', link:'../ContactPage/index.html'},
-    {title: 'WEB PLANS', link:'../WebPlans/index.html'},
+    {title: 'HOME', link:'https://thelalakei.github.io/WSOA3028A_1771810/index.html'},
+    {title: 'ABOUT', link:'https://thelalakei.github.io/WSOA3028A_1771810/AboutPage/index.html'},
+    {title: 'CHARACTER CONTENT', link:'https://thelalakei.github.io/WSOA3028A_1771810/CharacterContent/index.html'},
+    {title: 'THEORY', link:'https://thelalakei.github.io/WSOA3028A_1771810/TheoryContent/index.html'},
+    {title: 'CONTACT', link:'https://thelalakei.github.io/WSOA3028A_1771810/ContactPage/index.html'},
+    {title: 'WEB PLANS', link:'https://thelalakei.github.io/WSOA3028A_1771810/WebPlans/index.html'},
 ];
 
 const initialiseMenu = () => {
