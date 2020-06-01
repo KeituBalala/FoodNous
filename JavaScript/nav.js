9bd9b0c6eced47b7553baa0ff60327b076902897
thelalakei <thelalakei@gmail.com>
2020-06-01 16:12:39 +0200

    Update nav.js

--- JavaScript/nav.js
+++ JavaScript/nav.js
@@ -26,3 +26,11 @@ nav.appendChild (ul);
 
 document.addEventListener('DOMContentLoaded', () => initialiseMenu());
 
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
